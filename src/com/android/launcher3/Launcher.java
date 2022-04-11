a4cb8cda16b07efd5ebbd6649309dea4d2ddf805
Danny Lin <danny@kdrag0n.dev>
2022-04-10 18:38:45 -0700

    Add support for enabling taskbar on phones
    - Use new API to enable/disable SysUI taskbar integration
    - User-facing setting to control taskbar eligibility override on phones
    - Reload by restarting (live reload would be much more complicated)
    - Update SysUI integration state before reloading
    
    Depends on frameworks/base commit:
        SystemUI: Add API for runtime taskbar config on phones
    
    Change-Id: Id1889ca9b102f94cc0a1161f49de88428dcc0943

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -380,6 +380,8 @@ public class Launcher extends StatefulActivity<LauncherState> implements Launche
     protected InstanceId mAllAppsSessionLogId;
     private LauncherState mPrevLauncherState;
 
+    private boolean mPendingRestart;
+
     @Override
     @TargetApi(Build.VERSION_CODES.S)
     protected void onCreate(Bundle savedInstanceState) {
@@ -591,7 +593,7 @@ public class Launcher extends StatefulActivity<LauncherState> implements Launche
     public void onConfigurationChanged(Configuration newConfig) {
         int diff = newConfig.diff(mOldConfig);
         if ((diff & (CONFIG_ORIENTATION | CONFIG_SCREEN_SIZE)) != 0) {
-            onIdpChanged(false);
+            onIdpChanged(false, false);
         }
 
         mOldConfig.setTo(newConfig);
@@ -599,7 +601,11 @@ public class Launcher extends StatefulActivity<LauncherState> implements Launche
     }
 
     @Override
-    public void onIdpChanged(boolean modelPropertiesChanged) {
+    public void onIdpChanged(boolean modelPropertiesChanged, boolean taskbarChanged) {
+        if (taskbarChanged) {
+            mPendingRestart = true;
+        }
+
         initDeviceProfile(mDeviceProfile.inv);
         dispatchDeviceProfileChanged();
         reapplyUi();
@@ -1145,6 +1151,10 @@ public class Launcher extends StatefulActivity<LauncherState> implements Launche
         }
 
         TraceHelper.INSTANCE.endSection(traceToken);
+
+        if (mPendingRestart) {
+            System.exit(0);
+        }
     }
 
     @Override
